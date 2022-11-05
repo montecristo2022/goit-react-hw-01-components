6c4026e99dd8de5ab80bebb361b201b0c99df3b5
montecristo2022 <104501641+montecristo2022@users.noreply.github.com>
2022-11-05 22:13:52 +0200

    i need props
    left to finish only props.

--- src/components/FriendsList/friendsList.jsx
+++ src/components/FriendsList/friendsList.jsx
@@ -1,11 +1,21 @@
 import { FriendsImg, FriendsWrapper, FriendsStatus, FriendsNameText, OneFriendContainer } from './friendList.styled';
+import styles from './friends.module.css'
 
 export const FriendsList = ({ friends }) => {
   return (
     <FriendsWrapper>
       {friends.map(friend => (
         <OneFriendContainer key={friend.id}>
-          <FriendsStatus></FriendsStatus>
+
+
+
+{/* КАК СДЕЛАТЬ БЕЗ КЛАССНЕЙМ? */}
+          <FriendsStatus className={friend.isOnline ? styles.statisticStatusGreen : styles.statisticStatusRed}></FriendsStatus>
+
+
+
+
+
           <FriendsNameText>{friend.name}</FriendsNameText>
           <FriendsImg src={friend.avatar} alt="friend"></FriendsImg>
         </OneFriendContainer>
