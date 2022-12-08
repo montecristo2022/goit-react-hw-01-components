e1ea4d63310395a438a2b857b8898f4cf8b38d68
montecristo2022 <104501641+montecristo2022@users.noreply.github.com>
2022-12-08 20:37:22 +0200

    save

--- src/components/FriendsListItem/FriendsListItem.jsx
+++ src/components/FriendsListItem/FriendsListItem.jsx
@@ -1,25 +1,25 @@
-// import PropTypes from 'prop-types';
-// import {
-//   FriendsImg,
-//   FriendsStatus,
-//   FriendsNameText,
-//   OneFriendContainer,
-// } from './friendLitsItem.styled';
+import PropTypes from 'prop-types';
+import {
+  FriendsImg,
+  FriendsStatus,
+  FriendsNameText,
+  OneFriendContainer,
+} from './friendLitsItem.styled';
 
-// export const FriendsListItem = ({ avatar, name, isOnline}) => {
-//   return (
-//     <OneFriendContainer>
-//       <FriendsStatus isOnline={isOnline}></FriendsStatus>
+export const FriendsListItem = ({ avatar, name, isOnline}) => {
+  return (
+    <OneFriendContainer>
+      <FriendsStatus isOnline={isOnline}></FriendsStatus>
 
-//       <FriendsNameText>{name}</FriendsNameText>
-//       <FriendsImg src={avatar} alt="friend"></FriendsImg>
-//     </OneFriendContainer>
-//   );
-// };
+      <FriendsNameText>{name}</FriendsNameText>
+      <FriendsImg src={avatar} alt="friend"></FriendsImg>
+    </OneFriendContainer>
+  );
+};
 
-// FriendsListItem.propTypes = {
-//   avatar: PropTypes.string.isRequired,
-//   name: PropTypes.string.isRequired,
-//   isOnline: PropTypes.bool.isRequired,
+FriendsListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
 
-// };
+};
